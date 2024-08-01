0d0bb67981f05c3a62128fe675e0cba198538e40
Michael Byers <mikebyers24@gmail.com>
2024-08-01 18:53:28 -0400

    dashboard user profile styles.

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -58,7 +58,7 @@ const Dashboard = () => {
   return (
     <div className="flex flex-col items-center mt-10">
       {currentUser ? (
-        <Card className="max-w-md w-full text-white bg-black border-2 border-primary">
+        <Card className="max-w-md w-full my-12 text-white bg-black border-2 border-primary">
           <CardHeader>
             <CardTitle className="text-center text-primary font-bold text-3xl">
               FIND YOUR MATCH
